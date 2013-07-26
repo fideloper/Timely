76b8dda6daa71295a76c5c69f79221914a6a16bf
Chris Fidao <chris.fidao@digitalsurgeons.com>
2013-07-26 16:01:10 -0700

    made public methods much better

--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -9,7 +9,7 @@ class ToolsTest extends PHPUnit_Framework_TestCase {
 		$datetime = new DateTime('1984-11-12 23:59:00');
 		$timely = new Timely( $datetime );
 
-		$expected = $timely->time();
+		$expected = $timely->get();
 
 		$this->assertInstanceOf('DateTime', $expected);
 		$this->assertEquals('1984-11-12 23:59:00', $expected->format('Y-m-d H:i:s'));
@@ -20,32 +20,76 @@ class ToolsTest extends PHPUnit_Framework_TestCase {
 		$datetime = new DateTime('1984-11-12 23:59:00');
 		$timely = new Timely;
 
-		$timely->time($datetime);
+		$timely->set($datetime);
 
-		$expected = $timely->time();
+		$expected = $timely->get();
 
 		$this->assertInstanceOf('DateTime', $expected);
 		$this->assertEquals('1984-11-12 23:59:00', $expected->format('Y-m-d H:i:s'));
 	}
 
-	// public function testToUtc()
-	// {
+	public function testGetTimezone()
+	{
+		$datetime = new DateTime('1984-11-12 23:59:00', new DateTimeZone('America/New_York'));
+		
+		$timely = new Timely;
 
-	// }
+		// Expected
+		$expected = new DateTime('1984-11-12 23:59:00', new DateTimeZone('America/New_York'));
+		$expected->setTimezone( new DateTimeZone('America/Los_Angeles') );
 
-	// public function testToTimezone()
-	// {
+		// Minus three hours, Eastern to Pacific time
+		$tested = $timely->set($datetime)->get('America/Los_Angeles');
 
-	// }
+		$this->assertInstanceOf('DateTime', $tested);
+		$this->assertEquals($expected->format('Y-m-d H:i:s'), $tested->format('Y-m-d H:i:s'));
+	}
 
-	// public function testUtcOffset()
-	// {
+	public function testGetUtc()
+	{
+		$datetime = new DateTime('1984-11-12 23:59:00', new DateTimeZone('America/New_York'));
+		
+		$timely = new Timely;
 
-	// }
+		// Expected
+		$expected = new DateTime('1984-11-12 23:59:00', new DateTimeZone('America/New_York'));
+		$expected->setTimezone( new DateTimeZone('UTC') );
 
-	// public function testGmtOffset()
-	// {
+		// Plus five hours, Eastern to UTC
+		$tested = $timely->set($datetime)->getUtc();
 
-	// }
+		$this->assertInstanceOf('DateTime', $expected);
+		$this->assertEquals($expected->format('Y-m-d H:i:s'), $tested->format('Y-m-d H:i:s'));
+	}
+
+	/**
+     * @expectedException RuntimeException
+     */
+	public function testNotSettingFirstThrowsException()
+	{
+		$timely = new Timely;
+
+		$timely->get('America/New_York');
+	}
+
+	/**
+     * @expectedException InvalidArgumentException
+     */
+	public function testNotPassingTimezoneThrowsException()
+	{
+		$timely = new Timely;
+
+		$timely->set( new DateTime )->get(1234);
+	}
+
+	/**
+     * @expectedException Exception
+     */
+	public function testPassingInvalidTimezoneThrowsException()
+	{
+		$timely = new Timely;
+
+		$timely->set( new DateTime )->get('Nonexistent');
+	}
 
 }
